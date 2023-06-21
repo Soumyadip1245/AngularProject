51e5833cd44284995427c8049882d0a83c1044ea
unknown <soumyadeepdas2002@gmail.com>
2023-06-21 20:26:27 +0530

    Commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path')
 const Mean = require('./routes/usercontroller')
 var mongoose = require('mongoose')
 var cors = require('cors')
+const router = require('./routes/usercontroller')
 
 mongoose.connect('mongodb+srv://Soumyadip:20csu214@cluster0.jm2zckm.mongodb.net/AngularProject?retryWrites=true&w=majority').then(()=>{
     console.log("Database Connected")
@@ -15,6 +16,9 @@ mongoose.connect('mongodb+srv://Soumyadip:20csu214@cluster0.jm2zckm.mongodb.net/
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/mean',Mean)
+router.get('/',(req,res)=>{
+    res.send("Backend Working")
+})
 app.listen(port,()=>{
     console.warn(`Server Running At Port: `+port)
 })
